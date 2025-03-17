2cce3da85a6afc51f71e83afdeb25f036039df38
Naif Ghannam <dhdfaui@gmail.com>
2025-03-17 13:16:32 +0300

    Initial Commit

---
+++ SimpleAPICall/SimpleAPICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAPICallApp.swift
+//  SimpleAPICall
+//
+//  Created by Mac on 17/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAPICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
